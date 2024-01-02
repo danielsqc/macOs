957a294f38e70af9f00fa1b6536ee6381c682f45
Daniel <danielsqc@gmail.com>
2024-01-02 13:06:05 -0500

    Initial commit from M1

---
+++ karabiner/rules.ts
@@ -0,0 +1,377 @@
+import fs from "fs";
+import { KarabinerRules } from "./types";
+import { createHyperSubLayers, app, open } from "./utils";
+
+const rules: KarabinerRules[] = [
+  // Define the Hyper key itself
+  {
+    description: "Hyper Key (⌃⌥⇧⌘)",
+    manipulators: [
+      {
+        description: "Caps Lock -> Hyper Key",
+        from: {
+          key_code: "caps_lock",
+        },
+        to: [
+          {
+            key_code: "left_shift",
+            modifiers: ["left_command", "left_control", "left_option"],
+          },
+        ],
+        to_if_alone: [
+          {
+            key_code: "escape",
+          },
+        ],
+        type: "basic",
+      },
+      //      {
+      //        type: "basic",
+      //        description: "Disable CMD + Tab to force Hyper Key usage",
+      //        from: {
+      //          key_code: "tab",
+      //          modifiers: {
+      //            mandatory: ["left_command"],
+      //          },
+      //        },
+      //        to: [
+      //          {
+      //            key_code: "tab",
+      //          },
+      //        ],
+      //      },
+    ],
+  },
+  ...createHyperSubLayers({
+    // o = "Open" applications
+    o: {
+      a: app("Arc"),
+      b: app("Obsidian"),
+      c: app("Clockify Desktop"),
+      f: app("Finder"),
+      h: open(
+        "notion://www.notion.so/stellatehq/7b33b924746647499d906c55f89d5026"
+      ),
+      i: app("IntelliJ IDEA Ultimate"),
+      n: app("Notion"),
+      s: app("Slack"),
+      t: app("iterm"),
+      v: app("Visual Studio Code"),
+      w: app("WhatsApp"),
+    },
+
+    // w = "Window" via rectangle.app
+    w: {
+      semicolon: {
+        description: "Window: Hide",
+        to: [
+          {
+            key_code: "h",
+            modifiers: ["right_command"],
+          },
+        ],
+      },
+      y: {
+        description: "Window: First Third",
+        to: [
+          {
+            key_code: "left_arrow",
+            modifiers: ["right_option", "right_control"],
+          },
+        ],
+      },
+      k: {
+        description: "Window: Top Half",
+        to: [
+          {
+            key_code: "up_arrow",
+            modifiers: ["right_option", "right_command"],
+          },
+        ],
+      },
+      j: {
+        description: "Window: Bottom Half",
+        to: [
+          {
+            key_code: "down_arrow",
+            modifiers: ["right_option", "right_command"],
+          },
+        ],
+      },
+      o: {
+        description: "Window: Last Third",
+        to: [
+          {
+            key_code: "right_arrow",
+            modifiers: ["right_option", "right_control"],
+          },
+        ],
+      },
+      h: {
+        description: "Window: Left Half",
+        to: [
+          {
+            key_code: "left_arrow",
+            modifiers: ["right_option", "right_command"],
+          },
+        ],
+      },
+      l: {
+        description: "Window: Right Half",
+        to: [
+          {
+            key_code: "right_arrow",
+            modifiers: ["right_option", "right_command"],
+          },
+        ],
+      },
+      f: {
+        description: "Window: Full Screen",
+        to: [
+          {
+            key_code: "f",
+            modifiers: ["right_option", "right_command"],
+          },
+        ],
+      },
+      u: {
+        description: "Window: Previous Tab",
+        to: [
+          {
+            key_code: "tab",
+            modifiers: ["right_control", "right_shift"],
+          },
+        ],
+      },
+      i: {
+        description: "Window: Next Tab",
+        to: [
+          {
+            key_code: "tab",
+            modifiers: ["right_control"],
+          },
+        ],
+      },
+      n: {
+        description: "Window: Next Window",
+        to: [
+          {
+            key_code: "grave_accent_and_tilde",
+            modifiers: ["right_command"],
+          },
+        ],
+      },
+      b: {
+        description: "Window: Back",
+        to: [
+          {
+            key_code: "open_bracket",
+            modifiers: ["right_command"],
+          },
+        ],
+      },
+      // Note: No literal connection. Both f and n are already taken.
+      m: {
+        description: "Window: Forward",
+        to: [
+          {
+            key_code: "close_bracket",
+            modifiers: ["right_command"],
+          },
+        ],
+      },
+      d: {
+        description: "Window: Next display",
+        to: [
+          {
+            key_code: "right_arrow",
+            modifiers: ["right_control", "right_option", "right_command"],
+          },
+        ],
+      },
+    },
+
+    // s = "Numbers" It doesn't make sense, but it's easy to use
+    s: {
+      b: {
+        to: [
+          {
+            key_code: "0",
+          },
+        ],
+      },
+      n: {
+        to: [
+          {
+            key_code: "1",
+          },
+        ],
+      },
+      m: {
+        to: [
+          {
+            key_code: "2",
+          },
+        ],
+      },
+      comma: {
+        to: [
+          {
+            key_code: "3",
+          },
+        ],
+      },
+      j: {
+        to: [
+          {
+            key_code: "4",
+          },
+        ],
+      },
+      k: {
+        to: [
+          {
+            key_code: "5",
+          },
+        ],
+      },
+      l: {
+        to: [
+          {
+            key_code: "6",
+          },
+        ],
+      },
+      u: {
+        to: [
+          {
+            key_code: "7",
+          },
+        ],
+      },
+      i: {
+        to: [
+          {
+            key_code: "8",
+          },
+        ],
+      },
+      o: {
+        to: [
+          {
+            key_code: "9",
+          },
+        ],
+      },
+      period: {
+        to: [
+          {
+            key_code: "period",
+          },
+        ],
+      },
+      hyphen: {
+        to: [
+          {
+            key_code: "hyphen",
+          },
+        ],
+      },
+      delete_or_backspace: {
+        to: [
+          {
+            key_code: "delete_or_backspace",
+          },
+        ],
+      },
+    },
+
+    // Doesn't make sense, but it's easy to use
+    //
+    a: {
+      h: {
+        to: [{ key_code: "left_arrow" }],
+      },
+      j: {
+        to: [{ key_code: "down_arrow" }],
+      },
+      k: {
+        to: [{ key_code: "up_arrow" }],
+      },
+      l: {
+        to: [{ key_code: "right_arrow" }],
+      },
+      // Magicmove via homerow.app
+      m: {
+        to: [{ key_code: "f", modifiers: ["right_control"] }],
+      },
+      // Scroll mode via homerow.app
+      s: {
+        to: [{ key_code: "j", modifiers: ["right_control"] }],
+      },
+      d: {
+        to: [{ key_code: "d", modifiers: ["right_shift", "right_command"] }],
+      },
+      u: {
+        to: [{ key_code: "page_down" }],
+      },
+      i: {
+        to: [{ key_code: "page_up" }],
+      },
+    },
+
+    // c = Musi*c* which isn't "m" because we want it to be on the left hand
+    c: {
+      p: {
+        to: [{ key_code: "play_or_pause" }],
+      },
+      n: {
+        to: [{ key_code: "fastforward" }],
+      },
+      b: {
+        to: [{ key_code: "rewind" }],
+      },
+    },
+
+    // r = "Raycast"
+    r: {
+      l: open(
+        "raycast://extensions/stellate/mxstbr-commands/create-mxs-is-shortlink"
+      ),
+      e: open("raycast://extensions/raycast/emoji/search-emoji"),
+      c: open("raycast://extensions/raycast/raycast/confetti"),
+      a: open("raycast://extensions/raycast/raycast-ai/ai-chat"),
+      s: open("raycast://extensions/peduarte/silent-mention/index"),
+      h: open(
+        "raycast://extensions/raycast/clipboard-history/clipboard-history"
+      ),
+      1: open(
+        "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-1"
+      ),
+      2: open(
+        "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-2"
+      ),
+    },
+  }),
+];
+
+fs.writeFileSync(
+  "karabiner.json",
+  JSON.stringify(
+    {
+      global: {
+        show_in_menu_bar: false,
+      },
+      profiles: [
+        {
+          name: "Default",
+          complex_modifications: {
+            rules,
+          },
+        },
+      ],
+    },
+    null,
+    2
+  )
+);
