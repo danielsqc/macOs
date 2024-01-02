e2f8686ea6638f81ea6db774d4f87abb537908bf
Daniel <daniel@Daniels-MacBook-Pro.local>
2024-01-01 19:40:42 -0500

    Fixed Harpoon

--- .config/nvim/lua/daniel/plugins/harpoon.lua
+++ .config/nvim/lua/daniel/plugins/harpoon.lua
@@ -8,18 +8,18 @@ return {
   -- Keybindings
   local keymap = vim.keymap
 
-    keymap.set(
-      "n",
-      "<leader>hm",
-      "<cmd>lua require('harpoon.mark').add_file()<cr>",
-      { desc = "Mark file with harpoon" }
-    )
+    keymap.set("n", "<leader>hm", "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = "Mark files you want to revisit later on" })
     keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
-    keymap.set(
-      "n",
-      "<leader>hp",
-      "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
-      { desc = "Go to previous harpoon mark" }
-    )
+    keymap.set("n", "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", { desc = "Go to previous harpoon mark" })
+    keymap.set("n", "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", { desc = "View all project marks" })
+    keymap.set("n", "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", { desc = "Navigates to term 1" })
+    keymap.set("n", "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", { desc = "Navigates to term 2" })
+    keymap.set("n", "<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", { desc = "Navigates to term 3" })
+    keymap.set("n", "<leader>h4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", { desc = "Navigates to term 4" })
+    keymap.set("n", "<leader>h5", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", { desc = "Navigates to term 5" })
+    keymap.set("n", "<leader>h6", "<cmd>lua require('harpoon.ui').nav_file(6)<cr>", { desc = "Navigates to term 6" })
+    keymap.set("n", "<leader>h7", "<cmd>lua require('harpoon.ui').nav_file(7)<cr>", { desc = "Navigates to term 7" })
+    keymap.set("n", "<leader>h8", "<cmd>lua require('harpoon.ui').nav_file(8)<cr>", { desc = "Navigates to term 8" })
+    keymap.set("n", "<leader>h9", "<cmd>lua require('harpoon.ui').nav_file(9)<cr>", { desc = "Navigates to term 9" })
   end,
 }
